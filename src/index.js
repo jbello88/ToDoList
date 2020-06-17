00eb51e2b63a31db04fb1eb5ff3dc6a0638c06a0
mseeli <markus@seeli.eu>
2020-06-17 12:41:57 +0200

    removed var

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ let items = repo.getItems();
 console.log(items.length + " items were loaded from storage")
 
 // uncoment the following block to generate new items
-//let item = repo.createNewItem();
-//item.text = 'This is a test #' + item.id;
-//repo.addItem(item);
+//  let item = repo.createNewItem();
+// item.text = 'This is a test #' + item.id;
+// repo.addItem(item);
 
 
 
